ad47bd46c087118fde6ce2e6d8c877226b706a00
JacopoLombardi <lombajacopo@gmail.com>
2024-02-20 18:39:01 +0100

    [fix] eliminated toFixed everywhere and added only in prezzoFinale, changed any type of variables and edited Readme file

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Calcolo prezzo del Biglietto
 3. Calcoliamo un Prezzo Biglietto parziale in base ai Km inseriti.
 4. Se il passeggero è Under 18 si applica 20% di sconto al Prezzo Parziale.
 5. Se il passeggero è Over 65 si applica 40% sconto al Prezzo Parziale.
-6. il Prezzo Finale viene stampato in pagina HTML. 
+6. il Prezzo Finale viene modificato ottenedo 2 cifre decimali.
+7. il Prezzo Finale con 2 decimali viene poi stampato in pagina HTML. 
--- script.js
+++ script.js
@@ -7,9 +7,7 @@ console.log('Km--->', km, 'eta--->', age);
 const prezzoPerKm = 0.21;
 
 // calcolo il prezzo del biglietto al Km + aggiunta della seconda cifra decimale
-let prezzoParziale = km * prezzoPerKm;
-prezzoParziale = prezzoParziale.toFixed(2);
-
+const prezzoParziale = km * prezzoPerKm;
 console.log('prezzoPerKm--->', prezzoParziale);
 
 
@@ -17,27 +15,26 @@ let prezzoFinale = 0;
 
 // Casistiche di sconto Under e Over
 if (age < 18){
-   let scontoUnder = (prezzoParziale * 20) / 100;
-   scontoUnder = scontoUnder.toFixed(2);
+   const scontoUnder = (prezzoParziale * 20) / 100;
    prezzoFinale = prezzoParziale - scontoUnder;
 }
 else if (age > 65){
-   let scontoOver = (prezzoParziale * 40) / 100;
-   scontoOver = scontoOver.toFixed(2);
+   const scontoOver = (prezzoParziale * 40) / 100;
    prezzoFinale = prezzoParziale - scontoOver;
 }
 else{  // age compreso tra 18 e 65 anni 
    prezzoFinale = prezzoParziale;
 }
 
+prezzoFinale = prezzoFinale.toFixed(2);
 console.log('prezzoFinale--->', prezzoFinale);
-
-
-// inserisco nell'HTML il prezzoFinale
-document.getElementById('output-prezzofinale').innerHTML += `${prezzoFinale}`;
 
 
 // inserisco nell'HTML i valori scelti dall'utente: Km, età e il costo del biglitto parziale
 document.getElementById('output-kminseriti').innerHTML += `${km} Km`;
 document.getElementById('output-etainserita').innerHTML += `${age}`;
 document.getElementById('output-prezzoparziale').innerHTML += `${prezzoParziale}`;
+
+
+// inserisco nell'HTML il prezzoFinale
+document.getElementById('output-prezzofinale').innerHTML += `${prezzoFinale}`;
